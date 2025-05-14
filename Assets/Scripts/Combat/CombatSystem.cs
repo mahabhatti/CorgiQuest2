5a77be065a1073a1850439191f8c9650d0c80376
tuckercu <aatucker@chapman.edu>
2025-05-13 18:42:02 -0700

    CombatSystem progress

--- Assets/Scripts/Combat/CombatSystem.cs
+++ Assets/Scripts/Combat/CombatSystem.cs
@@ -1,10 +1,26 @@
 using UnityEngine;
 using static System.Math;
 
-public static class CombatSystem
+public class CombatSystem : MonoBehaviour
 {
+    public enum CombatState
+    {
+        Start,
+        PlayerTurn,
+        EnemyTurn,
+        Win,
+        Lose,
+        Flee,
+        Busy
+    }
+    
     public static int CalculateDamage(int attack, int defense)
     {
         return Max((attack - defense), 0);
+    }
+
+    public static int RunPlayerTurn()
+    {
+        return 1;
     }
 }
